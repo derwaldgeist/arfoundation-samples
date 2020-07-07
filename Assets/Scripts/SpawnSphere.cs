0ea81678cc3bcb5bc4c13961efb515c6dd984307
Tom Brückner <tom@mablear.com>
2020-07-07 12:15:44 +0200

    added reproduction code

---
+++ Assets/Scripts/SpawnSphere.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class SpawnSphere : MonoBehaviour
+{
+	public GameObject prefab;
+	public ARSessionOrigin origin;
+	public float spawnSize = 0.5f;
+
+	public void OnClick() {
+		var go = Instantiate(prefab);
+		var l = origin.transform.localPosition;
+		go.transform.localPosition = new Vector3(l.x, l.y, l.z + 2);
+		go.transform.localScale = new Vector3(spawnSize,spawnSize,spawnSize);
+		go.transform.parent = origin.transform;
+		go = Instantiate(prefab);
+		go.transform.localPosition = new Vector3(l.x, l.y, l.z - 2);
+		go.transform.localScale = new Vector3(spawnSize,spawnSize,spawnSize);
+		go.transform.parent = origin.transform;
+	}
+}
---
+++ Assets/Scripts/SwapCamera.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class SwapCamera : MonoBehaviour
+{
+	public ARSession arSession;
+	public ARCameraManager arCameraManager;
+	public ARFaceManager arFaceManager;
+
+	public void Swap() {
+		if (arCameraManager.currentFacingDirection == CameraFacingDirection.World) {
+			SetCameraFacingDirection(CameraFacingDirection.User);
+		} else {
+			SetCameraFacingDirection(CameraFacingDirection.World);
+		}
+	}
+
+	public void SetCameraFacingDirection(CameraFacingDirection direction) {
+		if (direction == CameraFacingDirection.User) {
+			arCameraManager.requestedFacingDirection = CameraFacingDirection.User;
+			arFaceManager.enabled = true;
+		} else {
+			arFaceManager.enabled = false;
+			arCameraManager.requestedFacingDirection = CameraFacingDirection.World;
+			arSession.requestedTrackingMode = TrackingMode.PositionAndRotation;
+		}
+	}
+}
